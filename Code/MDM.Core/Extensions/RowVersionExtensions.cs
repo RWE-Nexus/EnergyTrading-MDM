5efe799bbc887a6d2f4f340ed7120bc35bd8c04e
Simon Hanlon <simon.hanlon@rwe.com>
2014-03-06 18:19:50 +0000

    Added extension method to properly convert Timestamp byte array into an unsigned long Version that can be used for comparison

---
+++ Code/MDM.Core/Extensions/RowVersionExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace EnergyTrading.MDM.Extensions
+{
+    using System;
+
+    public static class RowVersionExtensions
+    {
+        public static ulong ToUnsignedLongVersion(this byte[] timestamp)
+        {
+            if (timestamp.Length != 8)
+            {
+                throw new ArgumentOutOfRangeException("timestamp", "must be an 8 byte array for ToVersion");
+            }
+            var newArray = new byte[8];
+            Array.Copy(timestamp, newArray, 8);
+            Array.Reverse(newArray);
+            return BitConverter.ToUInt64(newArray, 0);
+        }
+    }
+}
