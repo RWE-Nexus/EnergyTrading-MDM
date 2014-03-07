cfd7414513652343b03dc3a79b32d0fcdbad135d
Simon Hanlon <simon.hanlon@rwe.com>
2014-03-07 14:10:52 +0000

    updated extension to check endian nature before reversing bytes (although normally windows will be little endian in nature)

--- Code/MDM.Core/Extensions/RowVersionExtensions.cs
+++ Code/MDM.Core/Extensions/RowVersionExtensions.cs
@@ -11,8 +11,13 @@
                 throw new ArgumentOutOfRangeException("timestamp", "must be an 8 byte array for ToUnsignedLongVersion");
             }
             var newArray = new byte[8];
-            Array.Copy(timestamp, newArray, 8);
-            Array.Reverse(newArray);
+            Array.Copy(timestamp, newArray, 8); // copy so we don't alter the original timestamp supplied by the database
+            // if we are little endian (normally on windows we are) reverse the bytes to get correct conversion
+            if (BitConverter.IsLittleEndian) 
+            {
+                Array.Reverse(newArray);
+            }
+
             return BitConverter.ToUInt64(newArray, 0);
         }
     }
