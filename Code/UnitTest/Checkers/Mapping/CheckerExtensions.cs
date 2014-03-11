3433134c4751b11e949ad205830b2c9e91dc331e
Mike Borg <mike.borg@rwe.com>
2014-03-11 11:41:21 +0100

    Moving back some of the checkers into core

---
+++ Code/UnitTest/Checkers/Mapping/CheckerExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace EnergyTrading.MDM.Test.Checkers.Mapping
+{
+    using EnergyTrading;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    public static class CheckerExtensions
+    {
+        public static void Check(this DateRange expected, DateRange candidate)
+        {
+            Assert.AreEqual(expected.Start, candidate.Start, "Start differs");
+            Assert.AreEqual(expected.Finish, candidate.Finish, "Finish differs");
+        }
+    }
+}
