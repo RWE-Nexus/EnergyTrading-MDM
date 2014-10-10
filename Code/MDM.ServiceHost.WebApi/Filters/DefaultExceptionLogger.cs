95512cc9ffa1f093819e8db8cfe4d35bf9ca26d2
Mike Borg <mikaelborg72@yahoo.com>
2014-10-10 09:28:57 +0200

    Some changes to the exception logger to not always log as error

--- Code/MDM.ServiceHost.WebApi/Filters/DefaultExceptionLogger.cs
+++ Code/MDM.ServiceHost.WebApi/Filters/DefaultExceptionLogger.cs
@@ -3,11 +3,15 @@ using System.Text;
 using System.Web.Http.ExceptionHandling;
 using EnergyTrading.Extensions;
 using EnergyTrading.Logging;
+using EnergyTrading.Mdm.Services;
+using EnergyTrading.Validation;
+using MDM.ServiceHost.WebApi.Infrastructure.Exceptions;
 
 namespace MDM.ServiceHost.WebApi.Filters
 {
     /// <summary>
-    /// Logs all exceptions to the Logger framework being used
+    /// Logs all exceptions to the Logger framework being used, either as error or debug 
+    /// depending on the type of exception thrown
     /// </summary>
     public class DefaultExceptionLogger : ExceptionLogger
     {
@@ -15,7 +19,26 @@ namespace MDM.ServiceHost.WebApi.Filters
 
         public override void Log(ExceptionLoggerContext context)
         {
-            logger.ErrorFormat("[{0}] [{1}] failed, see error details below{2}{3}", context.Request.Method, context.Request.RequestUri, Environment.NewLine, ExceptionDetails(context.Exception));
+            if (ShouldLogAsError(context))
+            {
+                logger.ErrorFormat("[{0}] [{1}] failed, see error details below{2}{3}", context.Request.Method,
+                    context.Request.RequestUri, Environment.NewLine, ExceptionDetails(context.Exception));
+            }
+            else
+            {
+                logger.DebugFormat("[{0}] [{1}] failed, see error details below{2}{3}", context.Request.Method,
+                    context.Request.RequestUri, Environment.NewLine, ExceptionDetails(context.Exception));
+            }
+        }
+
+        private bool ShouldLogAsError(ExceptionLoggerContext context)
+        {
+            return (!(context.Exception is MdmFaultException) && 
+                    !(context.Exception is NotFoundException) &&
+                    !(context.Exception is ValidationException) &&
+                    !(context.Exception is NotImplementedException) &&
+                    !(context.Exception is VersionConflictException)
+                    );
         }
 
         private string ExceptionDetails(Exception contextException)
